9f2937a3ae02d59773f044ed3488e412d4594feb
Vladimir Pejovski <vpejovski@gmail.com>
2014-02-13 16:12:20 -0800

    Refactored the script to work with older version of python runtime. el5, el6, sles11

--- tools/vmware_tools_check.py
+++ tools/vmware_tools_check.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 '''
-Created on Jan 25, 2013
+Created on   : Jan 25, 2013
+Last updated : Feb 13, 2014
 
 @author: vpejovski
 
@@ -11,29 +12,46 @@ If newer kernel version is detected it will invoke vmware-tools-config.pl
 to compile modules for current kernel and will start the vmwaretoold
 process.
 '''
+
 import os
 import subprocess
 
 
+def write_kernel_version(kernel_version):
+    """Writes current kernel version to filesystem."""
+
+    status_file = open('/etc/vmware-tools/kernel_version', 'w')
+    status_file.write(kernel_version)
+    status_file.close()
+
+
+def read_kernel_version():
+    """Reads current kernel version to filesystem."""
+    status_file = open('/etc/vmware-tools/kernel_version', 'r')
+    current_version = status_file.readline().strip()
+    status_file.close()
+    return current_version
+
+
 def tool_config_required():
+    """
+    Compares last know (saved) kernel version with the one running.
+    If different it will write and re-configure vmware tools on the host.
+    """
     kernel_version = os.uname()[2]
 
     if os.path.exists('/etc/vmware-tools/'):
         if os.path.exists('/etc/vmware-tools/kernel_version'):
-            current_version = ''
-            with open('/etc/vmware-tools/kernel_version', 'r') as status_file:
-                current_version = status_file.readline().strip()
+            current_version = read_kernel_version()
 
             if kernel_version == current_version:
                 return False
             else:
-                with open('/etc/vmware-tools/kernel_version', 'w') as status_file:
-                    status_file.write(kernel_version)
+                write_kernel_version(kernel_version)
                 return True
         else:
-            with open('/etc/vmware-tools/kernel_version', 'w') as status_file:
-                status_file.write(kernel_version)
-                return True
+            write_kernel_version(kernel_version)
+            return True
 
     return False
 
